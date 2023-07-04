91c984f02c3ec4f4af770ca23206ddff9954679f
TimoteoGambella <timi.gambella@hotmail.com>
2023-07-04 12:03:10 -0300

    buttons

---
+++ src/components/buttons/Button.jsx
@@ -0,0 +1,24 @@
+import { faArrowRight, faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Button({number,text,status,screen,child1,child2,child3}){
+    return(
+        <div className={
+            `button
+            ${number===1?"button1":number===2?"button2":number===3?"button3":number===4?"button4":"button5"}
+            ${!status&&number!==3?"disabled":!status&&number===3?"specialDisabled":""
+            }`}
+            style={{cursor:screen==="desktop"?"pointer":"default"}}
+        >
+            {child1 && 
+                <FontAwesomeIcon icon={faBars} size="xl"/>
+            }
+            {child2 && 
+                <p className={screen==="mobile"?"font-mobile-14":"font-desktop-16"}>{text}</p>
+            }
+            {child3 &&
+                <FontAwesomeIcon icon={faArrowRight} size="xl"/>
+            }
+        </div>
+    )
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,19 @@
+import Button from "../components/buttons/Button";
+
 export default function Home(){
     return(
-        <div>
-            HOME
+        <div style={{backgroundColor:"coral"}}>
+            <Button number={1} text={"Button"} status={true} screen={"mobile"} child1={true} child2={true} child3={true}/>
+            <Button number={2} text={"Button"} status={true} screen={"mobile"} child1={true} child2={true} child3={true}/>
+            <Button number={3} text={"Button"} status={false} screen={"mobile"} child1={true} child2={true} child3={true}/>
+            <Button number={4} text={"Button"} status={false} screen={"mobile"}  child2={true} child3={true}/>
+            <Button number={5} text={"Button"} status={false} screen={"mobile"} child1={true} child2={true} child3={true}/>
+
+            <Button number={1} text={"Button"} status={true} screen={"desktop"} child1={true}  child3={true}/>
+            <Button number={2} text={"Button"} status={true} screen={"desktop"} child1={true} child2={true} child3={true}/>
+            <Button number={3} text={"Button"} status={false} screen={"desktop"} child1={true} child2={true} child3={true}/>
+            <Button number={4} text={"Button"} status={false} screen={"desktop"} child1={true} />
+            <Button number={5} text={"Button"} status={true} screen={"desktop"} child1={true} child2={true} child3={true}/>
         </div>
     )
 }
