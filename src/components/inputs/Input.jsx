bb6ceafb289edaf59f4da35a090ce4512f0deb0b
Matias Aguilera <aguileramati50@gmail.com>
2023-07-06 18:55:12 -0300

    estilos

--- src/components/inputs/Input.jsx
+++ src/components/inputs/Input.jsx
@@ -1,23 +1,23 @@
 import { TextField } from "@mui/material";
 
-export default function Input({label,error,helper,disabled,classes,handleChange,typeOfInput}){
-    return(
+export default function Input({ label,error,helper,disabled,classes,handleChange,typeOfInput }) {
+    return (
         <TextField
             variant="outlined"
             id={`outlined-basic`}
-            error={error&&!disabled}
+            error={error && !disabled}
             disabled={disabled}
             label={label}
             helperText={helper}
             type={typeOfInput}
             className={
                 `specialInput ${classes}
-                ${!error&&!disabled&&"active"}
-                ${error&&!disabled&&"error"}
-                ${!error&&disabled&&"disabled"}
-                ${error&&disabled&&"disabled"}`
+                ${!error && !disabled && "active"}
+                ${error && !disabled && "error"}
+                ${!error && disabled && "disabled"}
+                ${error && disabled && "disabled"}`
             }
-            onChangeCapture={(e)=>handleChange(e.target.value)}
+            onChangeCapture={(e) => handleChange(e.target.value)}
         />
     )
 }
