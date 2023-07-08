2007a90ad5afafaf1fa55a3fdfc5355e8fc61f13
Matias Aguilera <aguileramati50@gmail.com>
2023-07-08 16:41:43 -0300

    estilos

--- src/components/inputs/Input.jsx
+++ src/components/inputs/Input.jsx
@@ -1,23 +1,35 @@
 import { TextField } from "@mui/material";
+import { ThemeProvider,createTheme } from '@mui/material/styles';
 
 export default function Input({ label,error,helper,disabled,classes,handleChange,typeOfInput }) {
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#002F6C',
+            },
+        },
+    });
     return (
-        <TextField
-            variant="outlined"
-            id={`outlined-basic`}
-            error={error && !disabled}
-            disabled={disabled}
-            label={label}
-            helperText={helper}
-            type={typeOfInput}
-            className={
-                `specialInput ${classes}
-                ${!error && !disabled && "active"}
-                ${error && !disabled && "error"}
-                ${!error && disabled && "disabled"}
-                ${error && disabled && "disabled"}`
-            }
-            onChangeCapture={(e) => handleChange(e.target.value)}
-        />
+        <ThemeProvider theme={theme}>
+            <TextField
+                variant="outlined"
+                id={`outlined-basic`}
+                error={error && !disabled}
+                disabled={disabled}
+                label={label}
+                helperText={helper}
+                type={typeOfInput}
+                color="primary"
+                className={
+                    `specialInput ${classes}
+                    ${!error && !disabled && "active"}
+                    ${error && !disabled && "error"}
+                    ${!error && disabled && "disabled"}
+                    ${error && disabled && "disabled"}`
+                }
+                onChangeCapture={(e) => handleChange(e.target.value)}
+            />
+        </ThemeProvider>
     )
 }
